10a1f2440fb3e08fc6a09179a4804bbbef4d4a59
wals38 <wals.38100@gmail.com>
2018-08-24 11:37:33 +0200

    Chap 2 ending + update docx

--- firstScript.py
+++ firstScript.py
@@ -1,10 +1,12 @@
 # -*- conding: utf8 -*-
-from random import randint
+import random
+from turtle import *
 
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
     "Vi Veri Veniversum Vivux Vici", 
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre.",
+    "C'est le temps que tu as perdu pour ta rose qui fait ta rose si importante."
 ]
 
 characters = [
@@ -18,7 +20,7 @@ characters = [
 ]
 
 def show_random_list(list):
-	x = randint(0, 1)
+	x = random.randint(0, len(list)-1)
 	print(list[x])
 
 
@@ -28,13 +30,30 @@ for x in characters:
 print('')
 
 for x in characters:
-	print(x.capitalize())
+	print(characters.index(x), '. ', x.capitalize())
 
 print('')
-
 
 user_answer='Y'
 
 while user_answer=='Y':
 	show_random_list(quotes)
 	user_answer=input("Encore (Y/N) :? ")
+
+
+print(("{character} a dit : {quote}").format(character="Babar", quote="La vita é bella !"))
+
+def create_message(character, quote):
+    print("{} a dit : {}".format(character, quote))
+
+create_message("Wals", "Y'a pas de secret...")
+
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
